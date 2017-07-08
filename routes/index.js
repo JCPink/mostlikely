1e5293aeb3df501ce32cf0c075892598f16b4979
Lamine <songoku2019@gmail.com>
2017-07-08 11:28:33 +0100

    Added files

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var module_race = require("../lib/modules/race");
+
+exports.configure = function (app){
+	app.get('/', function(req, res){
+		res.render ('index.html', {title: 'Race'});
+	});
+	
+}
